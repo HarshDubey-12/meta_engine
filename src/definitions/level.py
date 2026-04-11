27ed75fa5536280c2f6312878906001607ef7fd0
HarshDubey-12 <feynman3253@gmail.com>
2026-04-11 12:00:00 +0530

    Problem level definitions

--- src/definitions/level.py
+++ src/definitions/level.py
@@ -1,1 +1,11 @@
 """Problem level definitions."""
+# Importing Utlities 
+from enum import Enum
+
+# Defining enum 
+class ComplexityLevel(Enum):
+    UNKNOWN = "unknown"
+    LEVEL_0 = "direct_formula"
+    LEVEL_1 = "multi_step_analytical"
+    LEVEL_2 = "calculus_simple_ode"
+    LEVEL_3 = "dynamic_simulation"
